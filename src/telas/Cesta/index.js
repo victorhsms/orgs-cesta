add9c6700592a583399b8cd5888fc3f26edae549
victorhsmsantos <victorhsmsantos@gmail.com>
2022-04-17 14:56:31 -0300

    Transformado o component lista em uma FlatList

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,23 +1,41 @@
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 
 import Topo from "./componentes/Topo";
 import Detalhes from "./componentes/Detalhes";
-import Itens from './componentes/Itens';
+import Item from './componentes/Item';
+import Texto from '../../componentes/Texto';
 
 export default ({ topo, detalhes, itens }) => {
   return (
-    <ScrollView>
-      <Topo {...topo} />
+    <>
+      <FlatList
+        data={ itens.lista }
+        renderItem={ Item }
+        keyExtractor={ ({ nome }) => nome }
+        ListHeaderComponent={() => (
+          <>
+            <Topo {...topo} />
       
-      <View style={estilos.cesta}>
-        <Detalhes {...detalhes} />
-        <Itens { ...itens } />
-      </View>
-    </ScrollView>
+            <View style={estilos.cesta}>
+              <Detalhes {...detalhes} />
+              <Texto style={ estilos.titulo }>{ itens.titulo }</Texto>
+            </View>
+          </>
+        )}
+      />
+    </>
   );
 }
 
 const estilos = StyleSheet.create({
+  titulo: {
+    color: '#464646',
+    fontWeight: 'bold',
+    marginTop: 32,
+    marginBottom: 8,
+    fontSize: 20,
+    lineHeight: 32,
+  },
   cesta: {
     paddingVertical: 8,
     paddingHorizontal: 16,
